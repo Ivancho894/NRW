5477dc1045fe40f8d2b84a5f9e6b1d0e2ba2dd77
ivancho894 <serralta.ivan@gmail.com>
2023-12-18 18:26:10 -0300

    First

---
+++ src/components/Todos_los_Barcos/SinBarcos.jsx
@@ -0,0 +1,14 @@
+
+
+
+export default function SinBarcos(){
+
+
+    return (
+        <div className="min-h-screen items-center justify-end p-40 w-full">
+        <div className="mt-8 ">
+            <h1>Lo sentimos no hay barcos por los que se ha filtrado. Por favor, vuelve a filtrar o modifica los criterios de búsqueda.</h1>
+        </div>
+    </div>
+    )
+}
